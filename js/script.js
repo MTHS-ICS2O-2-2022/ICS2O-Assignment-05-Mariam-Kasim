1d8821a77e11a36626c0ea40118f9cd8320a8698
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-24 21:46:02 +0000

    main

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mariam Kasim
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+/**
+ * This function adds up 1s until the number inputted
+ */
+function myButtonClicked() {
+  //input
+  const number = parseInt(document.getElementById("number1").value);
+
+  // process
+  let counter = 0;
+  let total = 0;
+
+  while (counter <= number) {
+    total = total + counter;
+    counter = counter + 1;
+  }
+
+  // output
+  document.getElementById("answer").innerHTML = "The sum of all positive numbers from 1 to " + number + " is " + total;
+}
