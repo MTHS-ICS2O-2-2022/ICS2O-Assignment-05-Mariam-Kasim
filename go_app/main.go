ba323760f751153e9cedac18d9727b77a343b125
Mariam-Kasim <124389357+Mariam-Kasim@users.noreply.github.com>
2023-05-24 23:03:31 +0000

    added go

---
+++ go_app/main.go
@@ -0,0 +1,32 @@
+// Created by: Mariam Kasim
+// Created on: March 2023
+//
+// This program finds does multiplication
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number1 int
+	var counter = 0
+	var total = 0
+
+	// input
+	fmt.Println("Let's do addition!")
+	fmt.Println("Enter the first number:")
+	fmt.Scanln(&number1)
+
+	// process
+	for counter <= number1 {
+		total = total + counter
+		counter = counter + 1
+	}
+
+	// output
+	fmt.Println("")
+	fmt.Println("The sum of all positive numbers from 0 to", number1, "is", total)
+	fmt.Println("\nDone.")
+}
